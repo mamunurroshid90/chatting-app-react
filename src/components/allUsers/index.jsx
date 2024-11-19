ef5889e00ae2628e4b8169f9d5735f74d72007d4
Mamunur Roshid <69715351+mamunurroshid90@users.noreply.github.com>
2024-11-20 01:17:56 +0600

    added send friend request

--- src/components/allUsers/index.jsx
+++ src/components/allUsers/index.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { AddUserIcon } from "../../../public/svg/AddUser";
-import { getDatabase, onValue, ref } from "firebase/database";
+import { getDatabase, onValue, push, ref, set } from "firebase/database";
 import { useSelector } from "react-redux";
 import { getDownloadURL, getStorage, ref as Ref } from "firebase/storage";
 import avatar2 from "../../../public/images/avatar2.png";
 
 const AllUsers = () => {
   const [users, setUsers] = useState([]);
+  const [friendReqList, setFriendReqList] = useState([]);
+  const [cancelRequest, setCancelRequest] = useState([]);
   const user = useSelector((user) => user.login.loggedIn);
   const db = getDatabase();
   const storage = getStorage();
@@ -41,6 +43,51 @@ const AllUsers = () => {
       });
     });
   }, [db, user.uid, storage]);
+
+  // console.log(users);
+
+  // sent friend request
+
+  const handleFriendRequest = (data) => {
+    console.log(data);
+    set(push(ref(db, "friendRequest")), {
+      senderName: user.displayName,
+      senderId: user.uid,
+      senderProfilePic: user.photoURL ?? "/public/images/avatar2.png",
+      receiverName: data.username,
+      receiverId: data.id,
+      receiverProfilePic: data.photoURL ?? "/public/images/avatar2.png",
+    });
+  };
+
+  // show friend request
+  useEffect(() => {
+    const starCountRef = ref(db, "friendRequest/");
+    onValue(starCountRef, (snapshot) => {
+      let reqArr = [];
+      snapshot.forEach((friendReq) => {
+        reqArr.push(friendReq.val().receiverId + friendReq.val().senderId);
+      });
+      setFriendReqList(reqArr);
+    });
+  }, [db]);
+
+  console.log(friendReqList);
+
+  // cancel friend request
+  useEffect(() => {
+    const starCountRef = ref(db, "friendRequest/");
+    onValue(starCountRef, (snapshot) => {
+      let cancelArr = [];
+      snapshot.forEach((friendReq) => {
+        cancelArr.push({ ...friendReq.val(), id: friendReq.key });
+      });
+      setCancelRequest(cancelArr);
+    });
+  }, [db]);
+
+  console.log(cancelRequest);
+
   return (
     <>
       <div>
@@ -50,24 +97,30 @@ const AllUsers = () => {
           placeholder="Search Users"
           className=" mt-6 mb-9 bg-[#F8F8F8] p-4 rounded-lg w-full outline-none"
         />
+
         <div className=" flex flex-col gap-2">
-          {users.map((user) => (
+          {users.map((item) => (
             <div className=" flex justify-between items-center">
               <div className=" flex items-center gap-3">
                 <div className=" w-12 h-12 rounded-full">
                   <img
-                    src={user.photoURL || avatar2}
+                    src={item.photoURL || avatar2}
                     alt="users-profile"
                     className=" w-full h-full object-cover rounded-full bg-slate-200"
                   />
                 </div>
                 <h3 className=" text-lg font-fontInter font-medium">
-                  {user.username}
+                  {item.username}
                 </h3>
               </div>
-              <div>
-                <AddUserIcon />
-              </div>
+              {friendReqList.includes(item.id + user.uid) ||
+              friendReqList.includes(user.uid + item.id) ? (
+                <button>Cancel Request</button>
+              ) : (
+                <div onClick={() => handleFriendRequest(item)}>
+                  <AddUserIcon />
+                </div>
+              )}
             </div>
           ))}
         </div>
--- src/components/friendRequest/index.jsx
+++ src/components/friendRequest/index.jsx
@@ -29,27 +29,6 @@ const FriendRequest = () => {
               </button>
             </div>
           </div>
-          <div className=" flex items-center justify-between">
-            <div className=" flex items-center gap-3">
-              <div className=" w-12 h-12 rounded-full">
-                <img
-                  src="/public/images/profile.png"
-                  alt="friend-request-profile"
-                />
-              </div>
-              <h3 className=" text-lg font-fontInter font-medium">
-                Rashedul Islam
-              </h3>
-            </div>
-            <div className=" flex items-center gap-1">
-              <button className=" bg-[#4A81D3] py-1 px-4 rounded text-white">
-                Accept
-              </button>
-              <button className=" bg-[#D34A4A] py-1 px-4 rounded text-white">
-                Reject
-              </button>
-            </div>
-          </div>
         </div>
       </div>
     </>
