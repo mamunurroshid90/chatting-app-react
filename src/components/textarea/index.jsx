62c6b720afb90a51079009148b17238af3221c64
Mamunur Roshid <69715351+mamunurroshid90@users.noreply.github.com>
2024-11-24 01:12:45 +0600

    added message box design

--- src/components/textarea/index.jsx
+++ src/components/textarea/index.jsx
@@ -13,8 +13,37 @@ const TextBox = () => {
           </div>
           <h3>Mizanur Rahman</h3>
         </div>
-        <div className=" h-[540px] w-full">
-          <div name="" id="" className=" h-full w-full outline-none"></div>
+        <div className=" h-[540px] w-full overflow-y-auto">
+          <div name="" id="" className=" h-full w-full">
+            <div className=" ml-auto w-[60%]">
+              <p className=" bg-black text-white p-3 rounded">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Laborum fugit earum inventore, cum laboriosam quidem aut
+                molestiae veniam quia itaque.
+              </p>
+            </div>
+            <div className=" mr-auto w-[60%] my-2">
+              <p className=" bg-blue-700 text-white p-3 rounded">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Laborum fugit earum inventore, cum laboriosam quidem aut
+                molestiae veniam quia itaque.
+              </p>
+            </div>
+            <div className=" ml-auto w-[60%] my-2 overflow-hidden">
+              <img
+                src="/public/images/demo-img-chat-1.jpg"
+                alt="demo-img"
+                className=" w-full h-full object-cover"
+              />
+            </div>
+            <div className=" mr-auto w-[60%] my-2 overflow-hidden mt-2">
+              <img
+                src="/public/images/demo-img-chat-2.jpg"
+                alt="demo-img"
+                className=" w-full h-full object-cover"
+              />
+            </div>
+          </div>
         </div>
         <div className=" flex justify-between items-center bg-[#f5f5f5] py-3 pl-3 pr-2 mt-2">
           <div className=" flex gap-3">
